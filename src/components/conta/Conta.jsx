841e67999b7ccff3012e562a3dacee91d0edb603
Salomao-Faleta <faletas41@gmail.com>
2024-03-15 23:33:03 -0300

    finalizando a conta
    Falta redirecionar para algum lugar os links

--- src/components/conta/Conta.jsx
+++ src/components/conta/Conta.jsx
@@ -1,7 +1,8 @@
 import './Conta.css';
 import { MdOutlinePermContactCalendar } from "react-icons/md";
-// import { IoMdContact } from "react-icons/io";
+import { IoMdContact } from "react-icons/io";
 import { CgCloseR } from "react-icons/cg";
+import { FcGoogle } from "react-icons/fc";
 import { useState } from 'react';
 
 export function Conta() {
@@ -33,6 +34,39 @@ export function Conta() {
 
                     <div className='close' onClick={closeToggleConta}>
                         <CgCloseR />
+                    </div>
+
+                    <h1 className='titulo'>
+                        <IoMdContact/>
+                        FIAT ID
+                    </h1>
+
+                    <div className='infos'>
+                        <p>Acesse sua área exclusiva</p>
+
+                        <ul>
+                            <li>Salve os carros montados</li>
+                            <li>Acompanhe suas negociações</li>
+                            <li>Salve seus carros na garagem digital</li>
+                            <li>Cadastre-se ou entre com login Fiat e Jeep</li>
+                        </ul>
+                    </div>
+
+
+                    <div className="login">
+                        <div className="lg google">
+                            <p> <FcGoogle/> Entar com Google</p>
+                        </div>
+
+                        <div className="lg facebook">
+                            <p>Entar com Facebook</p>
+                        </div>
+
+                        <div className="lg email">
+                            <p>Entar com E-mail</p>
+                        </div>
+
+
                     </div>
 
                    
