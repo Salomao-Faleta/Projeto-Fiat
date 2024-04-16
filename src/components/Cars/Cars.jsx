dcb7bc5c762968c5cfdf63d1e61b8b15a43b1bab
Salomao-Faleta <faletas41@gmail.com>
2024-04-16 00:34:59 -0300

    Part.3 - The cars
    Criando a estrutura dos carros e adicionando novos veículos

--- src/components/Cars/Cars.jsx
+++ src/components/Cars/Cars.jsx
@@ -1,98 +1,30 @@
 import './Cars.css';
+import { listOfCars } from '../../data/listOfCars';
 
 export function Cars() {
     return (
         <div className='container'>
-            <div className='containerCar'>
-                <img className='imageCar' src="https://www.fiat.com.br/content/dam/fiat/products/376/av1/0/2024/page/profile-webp/profile-806.webp" alt="" />
 
-                <h1 className='titleCar'>FASTBACK AUDACE TURBO <br/> 200 FLEX AT 2024</h1>
+            {listOfCars.map((listOfCars) => (
+                <>
+                    <div className='containerCar'>
+                        <img className='imageCar' src={listOfCars.image} alt="" />
 
-                <h3 className='descriptionCar'>T200 AUTOMÁTICO FLEX 5P</h3>
+                        <h1 className='titleCar'>{listOfCars.name}</h1>
 
-                <p className='priceCar'>POR R$ 135.990,00</p>
+                        <h3 className='descriptionCar'>{listOfCars.description}</h3>
 
-                <div className='buttonsArea'>
-                    <button className='button' id='Eq'>Eu quero</button>
-                    <button className='button' id='wpp'>Negócie por whastApp</button>
-                </div>
+                        <p className='priceCar'>POR R$ {listOfCars.price}</p>
 
-            </div>
-            <div className='containerCar'>
-                <img className='imageCar' src="https://www.fiat.com.br/content/dam/fiat/products/376/av1/0/2024/page/profile-webp/profile-806.webp" alt="" />
 
-                <h1 className='titleCar'>FASTBACK AUDACE TURBO <br/> 200 FLEX AT 2024</h1>
+                        <div className='buttonsArea'>
+                            <button className='button' id='Eq'>Eu quero</button>
+                            <button className='button' id='wpp'>Negócie por whastApp</button>
+                        </div>
+                    </div>
 
-                <h3 className='descriptionCar'>T200 AUTOMÁTICO FLEX 5P</h3>
-
-                <p className='priceCar'>POR R$ 135.990,00</p>
-
-                <div className='buttonsArea'>
-                    <button className='button' id='Eq'>Eu quero</button>
-                    <button className='button' id='wpp'>Negócie por whastApp</button>
-                </div>
-
-            </div>
-            <div className='containerCar'>
-                <img className='imageCar' src="https://www.fiat.com.br/content/dam/fiat/products/376/av1/0/2024/page/profile-webp/profile-806.webp" alt="" />
-
-                <h1 className='titleCar'>FASTBACK AUDACE TURBO <br/> 200 FLEX AT 2024</h1>
-
-                <h3 className='descriptionCar'>T200 AUTOMÁTICO FLEX 5P</h3>
-
-                <p className='priceCar'>POR R$ 135.990,00</p>
-
-                <div className='buttonsArea'>
-                    <button className='button' id='Eq'>Eu quero</button>
-                    <button className='button' id='wpp'>Negócie por whastApp</button>
-                </div>
-
-            </div>
-            <div className='containerCar'>
-                <img className='imageCar' src="https://www.fiat.com.br/content/dam/fiat/products/376/av1/0/2024/page/profile-webp/profile-806.webp" alt="" />
-
-                <h1 className='titleCar'>FASTBACK AUDACE TURBO <br/> 200 FLEX AT 2024</h1>
-
-                <h3 className='descriptionCar'>T200 AUTOMÁTICO FLEX 5P</h3>
-
-                <p className='priceCar'>POR R$ 135.990,00</p>
-
-                <div className='buttonsArea'>
-                    <button className='button' id='Eq'>Eu quero</button>
-                    <button className='button' id='wpp'>Negócie por whastApp</button>
-                </div>
-
-            </div>
-            <div className='containerCar'>
-                <img className='imageCar' src="https://www.fiat.com.br/content/dam/fiat/products/376/av1/0/2024/page/profile-webp/profile-806.webp" alt="" />
-
-                <h1 className='titleCar'>FASTBACK AUDACE TURBO <br/> 200 FLEX AT 2024</h1>
-
-                <h3 className='descriptionCar'>T200 AUTOMÁTICO FLEX 5P</h3>
-
-                <p className='priceCar'>POR R$ 135.990,00</p>
-
-                <div className='buttonsArea'>
-                    <button className='button' id='Eq'>Eu quero</button>
-                    <button className='button' id='wpp'>Negócie por whastApp</button>
-                </div>
-
-            </div>
-            <div className='containerCar'>
-                <img className='imageCar' src="https://www.fiat.com.br/content/dam/fiat/products/376/av1/0/2024/page/profile-webp/profile-806.webp" alt="" />
-
-                <h1 className='titleCar'>FASTBACK AUDACE TURBO <br/> 200 FLEX AT 2024</h1>
-
-                <h3 className='descriptionCar'>T200 AUTOMÁTICO FLEX 5P</h3>
-
-                <p className='priceCar'>POR R$ 135.990,00</p>
-
-                <div className='buttonsArea'>
-                    <button className='button' id='Eq'>Eu quero</button>
-                    <button className='button' id='wpp'>Negócie por whastApp</button>
-                </div>
-
-            </div>
+                </>
+            ))}
         </div>
     );
 }
---
+++ src/data/listOfCars.js
@@ -0,0 +1,77 @@
+// const produtos = [
+//     { nome: 'Camiseta', preco: 30 },
+//     { nome: 'Calça', preco: 50 },
+//     { nome: 'Sapato', preco: 100 },
+//     { nome: 'Jaqueta', preco: 80 }
+// ];
+// console.log(produtos);
+
+
+
+
+export const listOfCars = [
+    {
+        image: 'https://ofertas.fiat.com.br/content/dam/fiat/products/376/a11/0/2024/page/profile-webp/profile-806.webp',
+        name: 'FASTBACK AUDACE TURBO 200 FLEX AT 2024 2024/2024',
+        description: 'T200 AUTOMÁTICO FLEX 5P',
+        price:  "135.990,00"
+    },
+    {
+        image: 'https://ofertas.fiat.com.br/content/dam/fiat/products/226/1pe/1/2024/page/profile-webp/profile-176.webp',
+        name: 'TORO ENDURANCE TURBO 270 FLEX AT6 2024 2024/2024',
+        description: '2000 MANUAL FLEX',
+        price: "181.041,50"
+    },
+    {
+        image: "https://ofertas.fiat.com.br/content/dam/fiat/products/341/acs/0/2024/page/profile-webp/profile-806.webp",
+        name: 'MOBI LIKE 1.0 2024 2024/2024',
+        description: '1.0 MANUAL FLEX 4P',
+        price: "65.890,00"
+    },
+    {
+        image: "https://ofertas.fiat.com.br/content/dam/fiat/products/363/ahx/0/2023/page/profile-webp/profile-806.webp",
+        name: 'PULSE ABARTH TURBO 270 FLEX AT 4P 2023 - 2023/2023',
+        description: '1.3 MANUAL FLEX 4P',
+        price: "99.990,00"
+    },
+    {
+        image: "https://ofertas.fiat.com.br/content/dam/fiat/products/363/a1l/0/2024/page/profile-webp/profile-249.webp",
+        name: 'PULSE DRIVE 1.3 MT FLEX 4P 2024 - 2023/2024',
+        description: 'T270 AUTOMÁTICO FLEX 4P',
+        price: "139.990,00"
+    },
+    {
+        image: "https://ofertas.fiat.com.br/content/dam/fiat/products/295/agb/7/2024/page/profile-webp/profile-549.webp",
+        name: 'DUCATO CARGO 11,5M 2023/2024',
+        description: '2300 MANUAL DIESEL',
+        price: "218.231,20"
+    }
+
+
+
+
+
+]
+
+
+
+
+
+
+
+// <div className='containerCar'>
+//     <img className='imageCar' src="https://ofertas.fiat.com.br/content/dam/fiat/products/376/a11/0/2024/page/profile-webp/profile-806.webp" alt="" />
+
+//     <h1 className='titleCar'>FASTBACK AUDACE TURBO <br /> 200 FLEX AT 2024</h1>
+
+//     <h3 className='descriptionCar'>T200 AUTOMÁTICO FLEX 5P</h3>
+
+//     <p className='priceCar'>POR R$ 135.990,00</p>
+
+//     <div className='buttonsArea'>
+//         <button className='button' id='Eq'>Eu quero</button>
+//         <button className='button' id='wpp'>Negócie por whastApp</button>
+//     </div>
+// </div>
+
+
