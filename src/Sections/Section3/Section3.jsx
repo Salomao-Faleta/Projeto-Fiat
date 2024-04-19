d2649bf8d17c3cc7abbd4957cc17551ed3c12668
Salomao-Faleta <faletas41@gmail.com>
2024-04-18 21:47:09 -0300

    Part.4 - seções -> Seção3
    Criando a estrutura padrão

--- src/Sections/Section3/Section3.jsx
+++ src/Sections/Section3/Section3.jsx
@@ -1,8 +1,43 @@
 import './Section3.css';
-// import { ListSection3 } from '../../data/ListSection3';
+import { ListSection3 } from '../../data/ListSection3';
 
 export function Section3() {
     return (
-        <div></div>
+        <div className="grid-section3">
+
+            {ListSection3.map((ListSection3) => (
+                <>
+                    <div className="container-section3">
+
+                        <img className='image-section3' src={ListSection3.image} alt="Banner" />
+
+                        <h1 className='title-section3'>{ListSection3.title}</h1>
+
+                        <p className='description-section3'>{ListSection3.description}</p>
+
+                    </div>
+                </>
+            ))}
+        </div>
     );
 }
+
+
+
+
+
+// {ListSection3.map((ListSection3) => (
+//     <>
+//         <div className="item-image-section3">
+//             <img className='image-section3' src={ListSection3.image} alt="Banner" />
+//         </div>
+
+//         <div className="item-name-section3">
+//             <h1>{ListSection3.title}</h1>
+//         </div>
+
+//         <div className="item-description-section3">
+//             <p>{ListSection3.description}</p>
+//         </div>
+//     </>
+// ))}
