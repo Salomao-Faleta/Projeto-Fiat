a808dd520a2f2ec703ee5c54f125ff38350f7423
Salomao-Faleta <faletas41@gmail.com>
2024-04-18 22:30:38 -0300

    part.3 - Adicionando mais carros a lista

--- src/data/ListSection3.js
+++ src/data/ListSection3.js
@@ -14,4 +14,49 @@ export const ListSection3 = [
         title: "Serviços Financeiros",
         description: "Temos as melhores soluções financeiras para você. Pelo nosso financiamento, consórcio ou seguro, você encontra as melhores condições de prazos e parcelas. Vem se surpreender."
     },
+    {
+        image: 'https://www.fiat.com.br/content/dam/fiat/nova_home/brandcards/minibanner-Flua-v2.webp',
+        title: "Flua! Com a Fiat",
+        description: "Já pensou em ter um FIAT por assinatura? A Flua! é a nova forma de ter um 0 KM, com economia e liberdade para viver novas experiências."
+    },
+    {
+        image: 'https://www.fiat.com.br/content/dam/fiat/nova_home/brandcards/minibanner-concessionaria-v3.webp',
+        title: "Concessionárias",
+        description: "A Fiat tem concessionárias em todo o Brasil, encontre a mais próxima e venha fazer um test-drive e sentir o cheirinho de carro novo."
+    },
+    {
+        image: 'https://www.fiat.com.br/content/dam/fiat/nova_home/brandcards/minibanner-vendadireta-v3.webp',
+        title: "Vendas Diretas",
+        description: "A Fiat tem as melhores soluções e descontos para empresas, produtores rurais, taxistas e outras categorias de negócio."
+    },
+    {
+        image: 'https://www.fiat.com.br/content/dam/fiat/nova_home/brandcards/minibanner-recall-v3.webp',
+        title: "Recall",
+        description: "Possui um veículo Fiat convocado para recall? Esse espaço é para você tirar todas as suas dúvidas."
+    },
+    {
+        image: 'https://www.fiat.com.br/content/dam/fiat/nova_home/brandcards/minibanner-spoticar.webp',
+        title: "Fiat SemiNovos",
+        description: "Compre seu carro usado com tranquilidade, segurança e sem burocracias! A SPOTICAR é especialista no mercado de usados e todos seus veículos tem garantia."
+    },
+    {
+        image: 'https://www.fiat.com.br/content/dam/fiat/nova_home/brandcards/minibanner-fiatwear-2.webp',
+        title: "Fiatwear",
+        description: "A paixão que nos move chegou ao seu guarda-roupa. O fiatwear é uma plataforma de moda inspirada na mistura Brasil-Itália."
+    },
+    {
+        image: 'https://www.fiat.com.br/content/dam/fiat/nova_home/brandcards/minibanner-cons%C3%B3rcio-v4.webp',
+        title: "Fiat Serviços",
+        description: "Descubra todos os serviços que temos disponíveis e agende agora mesmo uma visita."
+    },
+    {
+        image: 'https://www.fiat.com.br/content/dam/fiat/nova_home/brandcards/minibanner-fiqueatento-v2.webp',
+        title: "Fique Atento ao golpe",
+        description: "Desconfie de qualquer oferta de veículos novos da linha FIAT com preços abaixo dos praticados do mercado. Eles podem ser golpe."
+    },
+    {
+        image: 'https://www.fiat.com.br/content/dam/fiat/nova_home/brandcards/mini-banner-comunita-jul.webp',
+        title: "Fiat Comunità",
+        description: "Com projetos na área social, de sustentabilidade e diversidade, o Fiat Comunità é a concretização do compromisso da Fiat em impactar positivamente a comunidade."
+    },
 ]
